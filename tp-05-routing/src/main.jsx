de2c150151b4b18cd5136dd83e795de3b9779b0d
RayaneMeyfr <rayanemeyfr@hotmail.com>
2025-09-18 16:37:38 +0200

    ajout css pour la navBar et mise en place du routing pour voir une categorie selectioner

--- tp-05-routing/src/main.jsx
+++ tp-05-routing/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import { BrowserRouter } from 'react-router-dom'
-import RoutingList from './RoutingList'
+import RoutingList from './routing-list/RoutingList'
 
 
 
--- tp-05-routing/src/routing-list/RoutingList.jsx
+++ tp-05-routing/src/routing-list/RoutingList.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import HomePage from './HomePage/HomePage'
-import ShowLibelleExpense from './show-libelle-expense/ShowLibelleExpense'
-import ShowAllCategories from './show-all-categories/showAllCategories'
+import HomePage from '../HomePage/HomePage'
+import ShowLibelleExpense from '../show-libelle-expense/ShowLibelleExpense'
+import ShowAllCategories from '../show-all-categories/showAllCategories'
 import { useDispatch, useSelector } from 'react-redux';
-import { removeAmount } from './store/store';
-import FormAddExpense from './form-add-expense/FormAddExpense';
-import { NavLink, Route, Routes } from 'react-router-dom';
-
+import { removeAmount } from '../store/store';
+import FormAddExpense from '../form-add-expense/FormAddExpense';
+import { NavLink, Outlet, Route, Routes } from 'react-router-dom';
+import './RoutingList.css'
 
 
 export default function RoutingList() {
@@ -28,7 +28,9 @@ export default function RoutingList() {
 
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/categories' element={<ShowAllCategories dataExpense={dataExpense}/>}/>
+        <Route path="/categories" element={<div><h2>Catégories</h2><Outlet /></div>}>
+            <Route index element={<ShowAllCategories dataExpense={dataExpense} />} />
+        </Route>
         <Route path='/libelles' element={<ShowLibelleExpense dataExpense={dataExpense} onDelete={handleDelete}/>}/>
         <Route path='/add' element={<FormAddExpense/>}/>
       </Routes>
